fa6cdf82493b4dae6e39f162ea5572c236a54ef1
raslaner115 <rassdhhl@gmail.com>
2026-06-28 13:46:48 +0300

    ,

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,7 @@
 class AppConstants {
-  // Use the default Firebase Hosting domain until your custom domain (hirehub.app) is verified and pointed to Firebase.
-  static const String privacyPolicyUrl = "https://hire-hub-fe6c4.web.app/privacy-policy";
-  static const String termsOfServiceUrl = "https://hire-hub-fe6c4.web.app/terms-of-service";
+  static const String privacyPolicyUrl =
+      "https://hiro-services.com/privacy-policy";
+  static const String termsOfServiceUrl =
+      "https://hiro-services.com/terms-of-service";
   static const String contactEmail = "support@hirehub.com";
 }
